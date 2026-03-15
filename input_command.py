63128e0cc6c2cae344fcb40f3b040d48d9028c3e
nikkarger <neota1210@gmail.com>
2026-03-15 14:23:58 +0700

    Create input_command.py

---
+++ input_command.py
@@ -0,0 +1,14 @@
+name= input("what your name")
+print("oh so your name is:",name)
+
+age = input("what is your age?")
+print("ph that sick your age is:",age)
+
+game= input("what is your fav game")
+print("Oh so your fav game is ",game,"oh thats my fav too")
+
+kid= input("what is you fav child")
+print("oh yeah my fav child is",kid)
+
+food= input("whatt your fav food")
+print("oh yeah my is",food,"too")
